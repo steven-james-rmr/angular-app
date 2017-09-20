f27b387a3d580678552e9ff21bd91cb0705a6a9f
steven-james-rmr <rockymtnrednecks@gmail.com>
2017-09-20 12:25:13 -0600

    Created Mock Data in my Angular 2 Application

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
 	moduleId: module.id,
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
 	templateUrl: 'documents.component.html'
 })
 export class DocumentsComponent {
-	
+  documents: Document[] = [
+  	{
+  	  title: "My First Doc",
+  	  description: 'asdfasdf asdfasd',
+  	  file_url: 'http://google.com',
+  	  updated_at: '09/20/2017',
+  	  image_url: 'http://google.com',	
+  	},
+  	{
+  	  title: "My Second Doc",
+  	  description: 'asdfasdf asdfasd',
+  	  file_url: 'http://google.com',
+  	  updated_at: '09/20/2017',
+  	  image_url: 'http://google.com',	
+  	},
+  	{
+  	  title: "My Third Doc",
+  	  description: 'asdfasdf asdfasd',
+  	  file_url: 'http://google.com',
+  	  updated_at: '09/20/2017',
+  	  image_url: 'http://google.com',	
+  	}
+  ]
 }
